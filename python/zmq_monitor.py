332fbedc95615bca411bb1c40ba5724d0d7a3e87
james <jamesd@beaglebone.(none)>
2014-04-24 05:20:18 +0000

    zmq_monitor update

--- python/zmq_monitor.py
+++ python/zmq_monitor.py
@@ -23,18 +23,25 @@ if __name__=='__main__':
         serial_baud = sys.argv[4]
         
     cont = zmq.Context()
-    m_imu = imu_monitor.serial_publisher(3,cont,zmq_port,serial_port,serial_baud)
+    m_imu = imu_monitor.serial_publisher(3,cont,zmq_port,serial_port,serial_baud,'imu1.log')
+    m_imu2 = imu_monitor.serial_publisher(3,cont,str(int(zmq_port)+2),serial_port,serial_baud, 'imu2.log')
     m_pressure = pressure_monitor.logger(s_type = 3,zmq_context = cont,port = str(int(zmq_port)+10),file_name = 'pressure.log')
 
     serial_subscriber = cont.socket(zmq.SUB)
     serial_subscriber.setsockopt(zmq.SUBSCRIBE,'')
-    serial_subscriber.connect("tcp://10.0.0.20:4000")
+    serial_subscriber.connect("tcp://192.168.0.201:4000")
+
+    requested = cont.sock(zmq.REQ)
+    requested.connect("tcp://192.168.0.201:4001")
+    requested.send('none')
+    start_stamp = requested.recv()
     
     while(True):
         message = serial_subscriber.recv_multipart()
 
-        m_imu.run(buffer = message[1],time_stamp = message[0])
-        m_pressure.run(buffer = message[2],time_stamp = message[0])
+        m_imu.run(buffer = message[0],time_stamp = start_stamp)
+        m_imu2.run(buffer = message[1],time_stamp = start_stamp)
+        m_pressure.run(buffer = message[2],time_stamp = start_stamp)
 
 
 
