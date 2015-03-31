75979375a7ede37e368b95a291094afa80b47877
jamesd <jamesd2@email.arizona.edu>
2015-03-31 14:42:17 -0700

    added pressure

--- main.cpp
+++ main.cpp
@@ -7,34 +7,9 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
-
-int main(int argc, char* argv[])
-{
-    if(argc <3)
-    {
-        std::cout << "Not enough argumnets: Port, Serial Port" << std::endl;
-        return 1;
-    }
-    std::string port = argv[1];
-    std::string serial_port = argv[2];
-    std::string bind_cmd = "tcp://*:";
-
-    zmq::context_t context(1);
-    zmq::socket_t socket(context,ZMQ_PUB);
-    socket.bind((bind_cmd+port).c_str());
-
-    int ser;
-    ser = open(serial_port.c_str(),O_RDWR| O_NOCTTY);
-    char recv_buffer[64];
-    char send_buffer[128];
-    memset(recv_buffer,'\0',sizeof(recv_buffer));
-
-
-    int bytes_recv = 0;
-    int n = 0;
-    while(1){
-        //std::cout << "Received: " << rx << std::endl;
-        bytes_recv = 0;
+int fill_buffer(int file,char * recv_buffer, char* send_buffer){
+        int bytes_recv = 0;
+        int n = 0;
         while(bytes_recv < 64)
         {
             n = read(ser,&recv_buffer,64);
@@ -46,9 +21,47 @@ int main(int argc, char* argv[])
             memcpy(&send_buffer[bytes_recv],recv_buffer,n);
             bytes_recv += n;
         }
+        return bytes_recv;
+}
+
+void send_buffer(zmq::socket_t& socket, char* buffer, int bytes_recv){
         zmq::message_t message(bytes_recv);
         memcpy((char*)message.data(),send_buffer,bytes_recv);
         socket.send(message);
+}
+    
+
+int main(int argc, char* argv[])
+{
+
+    zmq::context_t context(1);
+
+    zmq::socket_t socket_imu1(context,ZMQ_PUB);
+    zmq::socket_t socket_pressure(context,ZMQ_PUB);
+    socket_imu1.bind("tcp://*:4000");
+    socket_pressure.bind("tcp://*:4001");
+
+    int imu1;
+    imu1 = open("/dev/ttyO5",O_RDWR| O_NOCTTY);
+
+    int pressure;
+    pressure = open("/dev/ttyO1",O_RDWR| O_NOCTTY);
+
+    char recv_imu1_buffer[64];
+    char recv_pressure_buffer[64];
+
+    char imu1_buffer[128];
+    char pressure_buffer[128];
+
+
+    int bytes_recv_imu1 = 0;
+    int bytes_recv_pressure = 0;
+    while(1){
+        bytes_recv_imu1 = fill_buffer(imu1,recv_imu1_buffer,imu1_buffer);
+        bytes_recv_pressure =fill_buffer(pressure,recv_pressure_buffer,pressure_buffer);
+        send_buffer(socket_imu1,imu1_buffer,bytes_recv_imu1);
+        send_buffer(socket_pressure,pressure_buffer,bytes_recv_pressure);
+
     }
 
 }
