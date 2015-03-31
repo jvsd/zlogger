44738e0f222bbc16f2732872a16cc30125b7670f
jamesd <jamesd2@email.arizona.edu>
2015-03-31 15:51:06 -0700

    added pressure

--- main.cpp
+++ main.cpp
@@ -7,10 +7,10 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
-char[] fill_buffer(int& file,int& bytes_recv){
+std::vector<char> fill_buffer(int& file,int& bytes_recv){
         int n = 0;
         char recv_buffer[64];
-        char send_buffer[128];
+        std::vector<char> send_buffer[128];
 
         while(bytes_recv < 64)
         {
@@ -20,7 +20,8 @@ char[] fill_buffer(int& file,int& bytes_recv){
                 std::cout << "Failed to recv data." << n << std::endl;
                 break;
             }
-            memcpy(&send_buffer[bytes_recv],recv_buffer,n);
+            char * data = &send_buffer[0]
+            memcpy(data+bytes_recv,recv_buffer,n);
             bytes_recv += n;
             std::cout << "loop" << std::endl;
         }
@@ -28,10 +29,10 @@ char[] fill_buffer(int& file,int& bytes_recv){
         return send_buffer;
 }
 
-void send_buffer(zmq::socket_t* socket, char* buffer, int bytes_recv){
+void send_buffer(zmq::socket_t* socket, std::vector<char> buffer, int bytes_recv){
         std::cout << "sending" << std::endl;
         zmq::message_t message(bytes_recv);
-        memcpy((char*)message.data(),buffer,bytes_recv);
+        memcpy((char*)message.data(),&buffer[0],bytes_recv);
         socket->send(message);
 }
     
@@ -52,16 +53,10 @@ int main(int argc, char* argv[])
     int pressure;
     pressure = open("/dev/ttyO1",O_RDWR| O_NOCTTY);
 
-    char recv_imu1_buffer[64];
-    char recv_pressure_buffer[64];
 
-    memset(recv_imu1_buffer, '\0', sizeof recv_imu1_buffer);
-    memset(recv_pressure_buffer, '\0', sizeof recv_pressure_buffer);
-    char imu1_buffer[128];
-    char pressure_buffer[128];
+    std::vector<char> imu1_buffer[128];
+    std::vector<char> pressure_buffer[128];
 
-    memset(imu1_buffer, '\0', sizeof imu1_buffer);
-    memset(pressure_buffer, '\0', sizeof pressure_buffer);
 
     int bytes_recv_imu1 = 0;
     int bytes_recv_pressure = 0;
