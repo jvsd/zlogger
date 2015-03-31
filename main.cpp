7285ea1c84a74a254d1c7ffcf60391cf2171d8e7
jamesd <jamesd2@email.arizona.edu>
2015-03-31 16:40:22 -0700

    added pressure

--- main.cpp
+++ main.cpp
@@ -37,33 +37,33 @@ int main(int argc, char* argv[])
     zmq::socket_t socket_imu1(context,ZMQ_PUB);
     zmq::socket_t socket_pressure(context,ZMQ_PUB);
     socket_imu1.bind("tcp://*:4000");
-    socket_pressure.bind("tcp://*:4001");
+    //socket_pressure.bind("tcp://*:4001");
 
     int imu1;
     imu1 = open("/dev/ttyO5",O_RDWR| O_NOCTTY);
 
-    int pressure;
-    pressure = open("/dev/ttyO1",O_RDWR| O_NOCTTY);
+    //int pressure;
+    //pressure = open("/dev/ttyO1",O_RDWR| O_NOCTTY);
 
 
     std::string imu1_buffer;
-    std::string pressure_buffer;
+    //std::string pressure_buffer;
     imu1_buffer.reserve(128);
-    pressure_buffer.reserve(128);
+    //pressure_buffer.reserve(128);
 
 
     int bytes_recv_imu1 = 0;
-    int bytes_recv_pressure = 0;
+    //int bytes_recv_pressure = 0;
     while(1){
 
         bytes_recv_imu1 = 0;
-        bytes_recv_pressure = 0;
+      //  bytes_recv_pressure = 0;
 
         imu1_buffer = fill_buffer(imu1,bytes_recv_imu1);
-        pressure_buffer = fill_buffer(pressure,bytes_recv_pressure);
+        //pressure_buffer = fill_buffer(pressure,bytes_recv_pressure);
 
         s_send(socket_imu1,imu1_buffer);
-        s_send(socket_pressure,pressure_buffer);
+        //s_send(socket_pressure,pressure_buffer);
 
 
 
