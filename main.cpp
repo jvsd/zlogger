86f45985f6b2f52424828748ecdd8fb24121421d
jamesd <jamesd2@email.arizona.edu>
2015-04-02 17:13:09 -0700

    buffer sizes

--- main.cpp
+++ main.cpp
@@ -11,17 +11,20 @@
 #include <fcntl.h>
 #include <ctime>
 
+int SEND_BUF_SIZE = 512;
+int RECV_BUF_SIZE = 128;
+
 // ./main ran as default and python script ran with -5 works well. Must setup ports with python prior. also set cpu_Freq to performance.
 
 std::string fill_buffer(int& file,int& bytes_recv){
         int n = 0;
-        char recv_buffer[64];
+        char recv_buffer[RECV_BUF_SIZE];
         std::string send_buffer;
-        send_buffer.reserve(128);
+        send_buffer.reserve(SEND_BUF_SIZE);
 
-        while(bytes_recv < 64)
+        while(bytes_recv < RECV_BUF_SIZE)
         {
-            n = read(file,recv_buffer,64);
+            n = read(file,recv_buffer,RECV_BUF_SIZE);
             if(n < 0)
             {
                 std::cout << "Failed to recv data." << n << std::endl;
@@ -45,7 +48,6 @@ int main(int argc, char* argv[])
     socket_imu1.bind("ipc:///tmp/4000");
     //socket_pressure.bind("ipc:///tmp/4001");
 
-    //time_t currentTime;
     struct tm *localTime;
     timeval curTime;
 
@@ -58,8 +60,8 @@ int main(int argc, char* argv[])
 
     std::string imu1_buffer;
     std::string pressure_buffer;
-    imu1_buffer.reserve(128);
-    pressure_buffer.reserve(128);
+    imu1_buffer.reserve(SEND_BUF_SIZE);
+    pressure_buffer.reserve(SEND_BUF_SIZE);
 
 
 
@@ -71,28 +73,23 @@ int main(int argc, char* argv[])
         bytes_recv_imu1 = 0;
         bytes_recv_pressure = 0;
 
-        //time(&currentTime);
         gettimeofday(&curTime,NULL);
         localTime=localtime(&curTime.tv_sec);
 
 
-       int day = localTime->tm_mday;
-       int hour = localTime->tm_hour;
-       int min = localTime->tm_min;
-       int sec = localTime->tm_sec;
-       int millis = curTime.tv_usec / 1000;
+        int day = localTime->tm_mday;
+        int hour = localTime->tm_hour;
+        int min = localTime->tm_min;
+        int sec = localTime->tm_sec;
+        int millis = curTime.tv_usec / 1000;
 
         imu1_buffer = fill_buffer(imu1,bytes_recv_imu1);
         pressure_buffer = fill_buffer(pressure,bytes_recv_pressure);
-
-
 
         outTime << day << ":" << hour << ":" << min << ":" << sec << ":" << millis;
         s_sendmore(socket_imu1,outTime.str());
         s_sendmore(socket_imu1,imu1_buffer);
         s_send(socket_imu1,pressure_buffer);
-
-
 
     }
 
